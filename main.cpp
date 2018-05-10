42fcf73e696266e542b6d2983005bc899d0ee23a
Pospolity <pospolity@outlook.com>
2018-05-10 16:26:57 +0200

    add generation of csv files with data

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "helpers.h"
 #include "approximate.h"
+#include <fstream>
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
@@ -13,11 +14,46 @@ int main() {
 
     int m = 2;
 
+    std::ofstream doPliku;
+
     for (int i = 0; i < 50; i++)
         resultArr[i] = approximate(m, qArr[i]);
 
     for (int i = 0; i < 50; i++)
         std::cout << resultArr[i] << " " << std::endl;
+
+    for (int m = 1; m <= 8; m++){
+        std::string fname = "resultM";
+        fname += std::to_string(m);
+        fname += ".csv";
+
+        doPliku.open(fname);
+
+        for (int i = 0; i < 50; i++) {
+            resultArr[i] = approximate(m, qArr[i]);
+        }
+
+        doPliku << "x,";
+        for (int i = 0; i < 50; i++)
+            doPliku << xArr[i] << ",";
+        doPliku << std::endl;
+
+        doPliku << "q,";
+        for (int i = 0; i < 50; i++)
+            doPliku << qArr[i] << ",";
+        doPliku << std::endl;
+
+        doPliku << "f(x),";
+        for (int i = 0; i < 50; i++)
+            doPliku << resultArr[i] << ",";
+        doPliku << std::endl;
+
+
+        doPliku.close();
+    }
+
+
+
 
     return 0;
 }
